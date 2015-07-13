1acc06109e4727485cc9a9a561dbbef68010fb50
Junichi Kato <j5ik2o@gmail.com>
2015-07-13 09:29:38 +0900

    PFG-757: Added configurations to SkinnyOrm.

--- infra/src/main/scala/sbtdaogen/example/infrastructure/CrudMapper.scala
+++ infra/src/main/scala/sbtdaogen/example/infrastructure/CrudMapper.scala
@@ -4,6 +4,10 @@ import skinny.orm.SkinnyCRUDMapperWithId
 
 trait CrudMapper[Id, Entity] extends SkinnyCRUDMapperWithId[Id, Entity] {
 
+  override def useAutoIncrementPrimaryKey: Boolean = false
+
+  override def useExternalIdGenerator: Boolean = true
+
   def toNamedValues(record: Entity): Seq[(Symbol, Any)]
 
 }
